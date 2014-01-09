f07226eccf13a996b141cbb00bc6fedbbcfda70d
Hamilton Chapman <hamchapman@gmail.com>
2014-01-09 16:41:08 +0000

    Added days helper method

--- catchr/app/controllers/days_controller.rb
+++ catchr/app/controllers/days_controller.rb
@@ -1,10 +1,14 @@
 class DaysController < ApplicationController
 
+  include DaysHelper
+
   def index
-    @days = Day.find_by(user_id: current_user.id)
+    @days = Day.where(user_id: current_user.id)
   end
 
   def show
+    entries = get_all_entries_for_day(params[:id])
+    raise entries.inspect
     @day = Day.find(params[:id])
   end
 
