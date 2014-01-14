2e4adcb604b2fed726f8342332863d163c93ba8b
Hamilton Chapman <hamchapman@gmail.com>
2014-01-14 17:42:15 +0000

    Added some more tests and renamed what_pulse to rescue_time

--- spec/requests/rescue_time_spec.rb
+++ spec/requests/rescue_time_spec.rb
@@ -2,10 +2,11 @@ require 'spec_helper'
 
 describe "RescueTimeAPI" do 
 
-  it "returns daily data in JSON", :vcr, record: :once do
+  it "returns daily data in JSON object", :vcr, record: :once do
     uri = URI("https://www.rescuetime.com/anapi/data?key=B63EMB8ooHFTiaqAOMRwE402lyzpfFGrYOuIfiue&format=json")
     response = Net::HTTP.get(uri)
     expect(response).to include "\"row_headers\":[\"Rank\",\"Time Spent (seconds)\",\"Number of People\",\"Activity\",\"Category\",\"Productivity\"]"
+    expect(JSON.parse(response)['rows'].class).to eq Array
   end
   
 end
--- spec/support/vcr_setup.rb
+++ spec/support/vcr_setup.rb
@@ -1,4 +1,5 @@
 VCR.configure do |c|
+  c.ignore_localhost = true
   c.cassette_library_dir = 'spec/vcr'
   c.hook_into :webmock
 end
