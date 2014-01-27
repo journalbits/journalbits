2e57870a5b6e88aef28a9edfbd5b970eb229e62e
Hamilton Chapman <hamchapman@gmail.com>
2014-01-27 19:07:32 +0000

    Fixed code that add facebook photos to db

--- app/helpers/facebook_api_helper.rb
+++ app/helpers/facebook_api_helper.rb
@@ -6,16 +6,16 @@ module FacebookApiHelper
     User.all.each do |user|
       if user.facebook_oauth_token
         @graph = Koala::Facebook::API.new(user.facebook_oauth_token)
-        user_photos_on (Time.now - 1.day), @graph, user
+        save_photos_on (Time.now - 1.day), @graph, user
       end
     end
   end
 
-  def save_photos date, client, user
+  def save_photos_on date, client, user
     photos = user_photos_on date, client, user
     photos.each do |photo|
       unless FacebookPhotoEntry.exists?(user_id: user.id, photo_id: photo['id'])
-        FacebookPhotoEntry.create(user_id: user.id, time_created: date, photo_id: photo['id'], source_url: photo['source'], link_url: photo['link'], medium_url: photo['images'][photo['images'].count / 2])
+        FacebookPhotoEntry.create(user_id: user.id, time_created: date.to_s, photo_id: photo['id'], source_url: photo['source'], link_url: photo['link'], medium_url: photo['images'][photo['images'].count / 2]['source'])
       end
     end
   end
@@ -29,7 +29,5 @@ module FacebookApiHelper
   #   photos = client.get_connections("me", "videos")
   #   photos_on_date = photos.select { |photo| photo['created_time'][0..9] == date.to_s[0..9] }
   # end
-
-  
 
 end
