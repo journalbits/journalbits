5fe695f5d12bbff450725dc5dc7f728ddd2a090c
Hamilton Chapman <hamchapman@gmail.com>
2014-01-28 19:25:51 +0000

    Migrated instagram oauth db change

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140128164519) do
+ActiveRecord::Schema.define(version: 20140128182220) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -152,6 +152,7 @@ ActiveRecord::Schema.define(version: 20140128164519) do
     t.string   "whatpulse_username"
     t.string   "evernote_oauth_token"
     t.string   "evernote_token_expires_at"
+    t.string   "instagram_oauth_token"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
