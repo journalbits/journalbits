b291d511c6606daf7a5306dec543ed04403fd13d
Hamilton Chapman <hamchapman@gmail.com>
2014-01-30 15:12:26 +0000

    Refactored disgusting omniauth if-elsif stack into case statement and added in cron timings for rufus

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
 
   # Dirty methods that allow me to use current_user in a model
   class << self
-    def current_user=(user)
+    def current_user= user
       Thread.current[:current_user] = user
     end
 
@@ -15,55 +15,83 @@ class User < ActiveRecord::Base
     end
   end
 
-  def self.from_omniauth(auth)
-    if auth.provider == "twitter"
-      user = where(twitter_uid: auth.uid).first || check_for_non_twitter_login(auth)
-      user.twitter_oauth_token = auth.credentials.token
-      user.twitter_oauth_secret = auth.credentials.secret
-      user.save! if user.email != ""
-    elsif auth.provider == "fitbit"
-      user = current_user
-      user.fitbit_oauth_token = auth.credentials.token
-      user.fitbit_oauth_secret = auth.credentials.secret
-      user.save! if user.email != ""
-    elsif auth.provider == "pocket"
-      user = current_user
-      user.pocket_oauth_token = auth.credentials.token
-      user.save! if user.email != ""
-    elsif auth.provider == "rdio"
-      user = current_user
-      user.rdio_oauth_token = auth.credentials.token
-      user.rdio_oauth_secret = auth.credentials.secret
-      user.save! if user.email != ""
-    elsif auth.provider == "facebook"
-      user = current_user
-      user.facebook_oauth_token = auth.credentials.token
-      user.facebook_token_expires_at = auth.credentials.expires_at
-      user.save! if user.email != ""
-    elsif auth.provider == "evernote"
-      user = current_user
-      user.evernote_oauth_token = auth.credentials.token
-      user.evernote_token_expires_at = Time.now + 1.year
-      user.save! if user.email != ""
-    elsif auth.provider == "instagram"
-      user = current_user
-      user.instagram_oauth_token = auth.credentials.token
-      user.instagram_uid = auth.uid
-      user.save! if user.email != ""
-    elsif auth.provider == "instapaper"
-      user = current_user
-      user.instapaper_oauth_token = auth.credentials.token
-      user.instapaper_oauth_secret = auth.credentials.secret
-      user.save! if user.email != ""
-    elsif auth.provider == "lastfm"
-      user = current_user
-      user.lastfm_username = auth.credentials.name
-      user.save! if user.email != ""
+  def self.from_omniauth auth
+    case auth.provider
+      when "twitter" then process_for_twitter auth
+      when "fitbit" then process_for_fitbit auth
+      when "pocket" then process_for_pocket auth
+      when "rdio" then process_for_rdio auth
+      when "facebook" then process_for_facebook auth
+      when "evernote" then process_for_evernote auth
+      when "instagram" then process_for_instagram auth
+      when "instapaper" then process_for_instapaper auth
+      when "lastfm" then process_for_lastfm auth
     end
     user
   end
 
-  def self.check_for_non_twitter_login(auth)
+  def process_for_twitter auth
+    user = where(twitter_uid: auth.uid).first || check_for_non_twitter_login(auth)
+    user.twitter_oauth_token = auth.credentials.token
+    user.twitter_oauth_secret = auth.credentials.secret
+    user.save! if user.email != ""
+  end
+
+  def process_for_fitbit auth
+    user = current_user
+    user.fitbit_oauth_token = auth.credentials.token
+    user.fitbit_oauth_secret = auth.credentials.secret
+    user.save! if user.email != ""
+  end
+
+  def process_for_pocket auth
+    user = current_user
+    user.pocket_oauth_token = auth.credentials.token
+    user.save! if user.email != ""
+  end
+
+  def process_for_rdio auth
+    user = current_user
+    user.rdio_oauth_token = auth.credentials.token
+    user.rdio_oauth_secret = auth.credentials.secret
+    user.save! if user.email != ""
+  end
+
+  def process_for_facebook auth
+    user = current_user
+    user.facebook_oauth_token = auth.credentials.token
+    user.facebook_token_expires_at = auth.credentials.expires_at
+    user.save! if user.email != ""
+  end
+
+  def process_for_evernote auth
+    user = current_user
+    user.evernote_oauth_token = auth.credentials.token
+    user.evernote_token_expires_at = Time.now + 1.year
+    user.save! if user.email != ""
+  end
+
+  def process_for_instagram auth
+    user = current_user
+    user.instagram_oauth_token = auth.credentials.token
+    user.instagram_uid = auth.uid
+    user.save! if user.email != ""
+  end
+
+  def process_for_instapaper auth
+    user = current_user
+    user.instapaper_oauth_token = auth.credentials.token
+    user.instapaper_oauth_secret = auth.credentials.secret
+    user.save! if user.email != ""
+  end
+
+  def process_for_lastfm auth
+    user = current_user
+    user.lastfm_username = auth.credentials.name
+    user.save! if user.email != ""
+  end
+
+  def self.check_for_non_twitter_login auth
     if current_user != nil && current_user.provider != "twitter"
       save_twitter_data_for_current_user(auth)
     else
@@ -71,7 +99,7 @@ class User < ActiveRecord::Base
     end
   end
 
-  def self.save_twitter_data_for_current_user(auth)
+  def self.save_twitter_data_for_current_user auth
     current_user.twitter_oauth_token = auth.credentials.token
     current_user.twitter_oauth_secret = auth.credentials.secret
     current_user.twitter_username = auth.info.nickname
@@ -80,7 +108,7 @@ class User < ActiveRecord::Base
     current_user
   end
 
-  def self.create_from_omniauth(auth)
+  def self.create_from_omniauth auth
     create do |user|
       user.provider = auth.provider
       user.twitter_uid = auth.uid
@@ -90,7 +118,7 @@ class User < ActiveRecord::Base
     end
   end
 
-  def self.new_with_session(params, session)
+  def self.new_with_session params, session
     if session["devise.user_attributes"]
       new session["devise.user_attributes"] do |user|
         user.attributes = params
@@ -105,7 +133,7 @@ class User < ActiveRecord::Base
     super && provider.blank?
   end
 
-  def update_with_password(params, *options)
+  def update_with_password params, *options
     if encrypted_password.blank?
       update_attributes(params, *options)
     else
--- config/initializers/api_scheduler.rb
+++ config/initializers/api_scheduler.rb
@@ -16,9 +16,25 @@ include LastfmApiHelper
 
 scheduler = Rufus::Scheduler.new
 
-# scheduler.cron("0 0 * * *") do
+# THIS ONE IS FOR 00:01 EVERY DAY
+scheduler.cron("0 1 0 1/1 * ? *") do
+  # twitter_data
+  # github_data
+  # wunderlist_data
+  # fitbit_data
+  # pocket_data
+  # facebook_data
+  # whatpulse_data
+  # evernote_data
+  # instagram_data
+  # instapaper_data
+  # lastfm_data
+end
 
-# end 
+# THIS ONE IS FOR 23:55 EVERY DAY
+scheduler.cron("0 55 23 1/1 * ? *") do
+  # rescue_time_data
+end
 
 scheduler.every("15s") do
   # twitter_data
