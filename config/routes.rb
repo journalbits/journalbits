73feadb357d42f4690ba3a9f9407e7e7c1fcc864
Hamilton Chapman <admin@journalbits.net>
2014-06-23 19:16:09 +0100

    Reordered routes

--- config/routes.rb
+++ config/routes.rb
@@ -30,22 +30,37 @@ JournalBits::Application.routes.draw do
   resources :users,  path: "" do
     resources :days, path: ""
 
-    resources :twitter_entries
-    resources :rescue_time_entries
-    resources :github_entries
-    resources :fitbit_weight_entries
+    resources :evernote_entries
+    resources :facebook_photo_entries
     resources :fitbit_activity_entries
     resources :fitbit_sleep_entries
-    resources :wunderlist_entries
-    resources :lastfm_entries
+    resources :fitbit_weight_entries
+    resources :github_entries
+    resources :health_graph_entries
     resources :instapaper_entries
     resources :instagram_entries
-    resources :evernote_entries
-    resources :whatpulse_entries
-    resources :facebook_photo_entries
-    resources :pocket_entries
-    resources :health_graph_entries
+    resources :lastfm_entries
     resources :moves_entries
+    resources :pocket_entries
+    resources :rescue_time_entries
+    resources :twitter_entries
+    resources :whatpulse_entries
+    resources :wunderlist_entries
+
+    resources :evernote_accounts
+    resources :facebook_accounts
+    resources :fitbit_accounts
+    resources :github_accounts
+    resources :health_graph_accounts
+    resources :instagram_accounts
+    resources :instapaper_accounts
+    resources :lastfm_accounts
+    resources :moves_accounts
+    resources :pocket_accounts
+    resources :rescue_time_accounts
+    resources :twitter_accounts
+    resources :whatpulse_accounts
+    resources :wunderlist_accounts
   end
 
   namespace :api, defaults: {format: 'json'} do
@@ -54,22 +69,22 @@ JournalBits::Application.routes.draw do
       resources :user do
         resources :days
 
-        resources :twitter_entries
-        resources :rescue_time_entries
-        resources :github_entries
-        resources :fitbit_weight_entries
+        resources :evernote_entries
+        resources :facebook_photo_entries
         resources :fitbit_activity_entries
         resources :fitbit_sleep_entries
-        resources :wunderlist_entries
-        resources :lastfm_entries
+        resources :fitbit_weight_entries
+        resources :github_entries
+        resources :health_graph_entries
         resources :instapaper_entries
         resources :instagram_entries
-        resources :evernote_entries
-        resources :whatpulse_entries
-        resources :facebook_photo_entries
-        resources :pocket_entries
-        resources :health_graph_entries
+        resources :lastfm_entries
         resources :moves_entries
+        resources :pocket_entries
+        resources :rescue_time_entries
+        resources :twitter_entries
+        resources :whatpulse_entries
+        resources :wunderlist_entries
       end
     end
   end
