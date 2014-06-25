533167033e8d8966d209b5914a9199872238fef7
Hamilton Chapman <admin@journalbits.net>
2014-06-25 19:35:48 +0100

    Translated 2 more processors to workers

---
+++ app/workers/moves_worker.rb
@@ -0,0 +1,52 @@
+require 'net/https'
+require 'moves'
+
+class MovesWorker
+  include Sidekiq::Worker
+  sidekiq_options queue: "external_api"
+
+  def perform date, user_id
+    # user = User.find(user_id)
+    # accounts = user.facebook_accounts.select { |a| a.activated }
+    accounts = MovesAccount.where(user_id: user_id, activated: true)
+    accounts.each do |account|
+      client = create_client_for account
+      save_media_on date, client, user_id
+    end
+  end
+
+  def create_client_for account
+    Moves::Client.new(account.oauth_token)
+  end
+
+  def user_summary_on date, client
+    formatted_date = date.to_s[0..9]
+    summary = client.daily_summary(formatted_date)[0]
+  end
+
+  def save_media_on date, client, user_id
+    summary = user_summary_on date, client
+    unless MovesEntry.exists?(user_id: user_id, date: date.to_s[0..9], activity: "total")
+      MovesEntry.create(
+        user_id: user_id,
+        date: date.to_s[0..9],
+        activity: "total",
+        calories: summary['caloriesIdle']
+      )
+    end
+    activities = summary['summary']
+    activities.each do |activity|
+      unless MovesEntry.exists?(user_id: user_id, date: date.to_s[0..9], activity: activity['activity'])
+        MovesEntry.create(
+          user_id: user_id,
+          date: date.to_s[0..9],
+          activity: activity['activity'],
+          duration: activity['duration'],
+          distance: activity['distance'],
+          steps: activity['steps'],
+          calories: activity['calories']
+        )
+      end
+    end
+  end
+end
---
+++ app/workers/pocket_worker.rb
@@ -0,0 +1,42 @@
+require "net/https"
+
+class PocketWorker
+  include Sidekiq::Worker
+  sidekiq_options queue: "external_api"
+
+  def perform date, user_id
+    accounts = PocketAccount.where(user_id: user_id, activated: true)
+    accounts.each do |account|
+      oauth_token = account.oauth_token
+      save_links_on date, oauth_token, user_id
+    end
+  end
+
+  def user_added_links_on date, oauth_token
+    consumer_key = ENV['POCKET_CONSUMER_KEY']
+    unix_date = date.to_time.to_i
+    uri = URI("https://getpocket.com/v3/get?access_token=#{oauth_token}&consumer_key=#{consumer_key}&since=#{unix_date}")
+    req = Net::HTTP::Get.new uri
+    response = Net::HTTP.start(uri.hostname, uri.port, :use_ssl => true) do |http|
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      http.ssl_version = :SSLv3
+      http.request(req)
+    end
+    JSON.parse(response.body)['list']
+  end
+
+  def save_links_on date, oauth_token, user_id
+    links = user_added_links_on date, oauth_token
+    links.each do |link|
+      unless PocketEntry.exists?(item_id: link[1]['item_id'])
+        PocketEntry.create(
+          date: date.to_s[0..9],
+          user_id: user_id,
+          item_id: link[1]['item_id'],
+          title: link[1]['resolved_title'],
+          url: link[1]['given_url']
+        )
+      end
+    end
+  end
+end
