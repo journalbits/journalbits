e38e3574855088c0cb5f5a0b49d55993019bcfce
Hamilton Chapman <admin@journalbits.net>
2014-06-26 08:56:08 +0100

    Fixed user factory error in tests

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,7 +1,7 @@
 require 'factory_girl_rails'
 
 FactoryGirl.define do
-  
+
   # factory :user do
   #   sequence(:email){ |n| "james#{n}@me.com" }
   #   password "12345678"
@@ -9,14 +9,16 @@ FactoryGirl.define do
   # end
 
   factory :user do
-    email "hamchapman@gmail.com"
-    provider "twitter" 
-    twitter_uid "83189257" 
-    twitter_username "hamchapman" 
-    twitter_oauth_token "83189257-XRSAxVQEbrjRBoyEZnHo5QVMA1EfVSRYuSwqva6pb" 
-    twitter_oauth_secret "zeAJFnd7fZ4IOZjB0AopBABFCycfhuGHnImvcuT4eUCCD"
-    rescue_time_key "B63EMB8ooHFTiaqAOMRwE402lyzpfFGrYOuIfiue"
-    github_access_token "418c91672f74323129338e7bf3a3e41e97cecdf4"
+    email 'hamchapman@gmail.com'
+    provider 'twitter'
+    username 'hamchapman'
+    slug 'hamchapman'
+    # twitter_uid "83189257"
+    # twitter_username "hamchapman"
+    # twitter_oauth_token "83189257-XRSAxVQEbrjRBoyEZnHo5QVMA1EfVSRYuSwqva6pb"
+    # twitter_oauth_secret "zeAJFnd7fZ4IOZjB0AopBABFCycfhuGHnImvcuT4eUCCD"
+    # rescue_time_key "B63EMB8ooHFTiaqAOMRwE402lyzpfFGrYOuIfiue"
+    # github_access_token "418c91672f74323129338e7bf3a3e41e97cecdf4"
   end
 
 end
