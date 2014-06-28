709a0da3f8087dd018f4734ba3ae929db723b349
Hamilton Chapman <admin@journalbits.net>
2014-06-28 16:06:37 +0100

    Added reauth for facebook accounts as well

--- app/workers/notify_reauth_worker.rb
+++ app/workers/notify_reauth_worker.rb
@@ -9,6 +9,12 @@ class SuperWorker
           notify user.id, 'Evernote', expires_at
         end
       end
+      user.facebook_accounts.each do |acc|
+        expires_at = acc.token_expires_at
+        if expires_at < (Time.now + 1.month)
+          notify user.id, 'Facebook', expires_at
+        end
+      end
     end
   end
 
