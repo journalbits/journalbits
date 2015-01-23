46ce13e795e87dd440da4e4d0cc8e1777d7f5a4f
Hamilton Chapman <admin@journalbits.net>
2015-01-23 21:47:05 +0500

    Changed pocketworker to use pocket-ruby gem instead of net/https

--- app/workers/services/pocket_worker.rb
+++ app/workers/services/pocket_worker.rb
@@ -1,4 +1,4 @@
-require "net/https"
+require 'pocket'
 
 class PocketWorker
   include Sidekiq::Worker
@@ -7,37 +7,37 @@ class PocketWorker
   def perform date, user_id
     accounts = PocketAccount.where(user_id: user_id, activated: true)
     accounts.each do |account|
-      oauth_token = account.oauth_token
-      save_links_on date, oauth_token, user_id, account.id
+      client = create_client_for account
+      save_bookmarks_created_on date, client, user_id, account.id
     end
   end
 
-  def user_added_links_on date, oauth_token
-    consumer_key = ENV['POCKET_CONSUMER_KEY']
-    unix_date = date.to_time.to_i
-    uri = URI("https://getpocket.com/v3/get?access_token=#{oauth_token}&consumer_key=#{consumer_key}&since=#{unix_date}")
-    req = Net::HTTP::Get.new uri
-    response = Net::HTTP.start(uri.hostname, uri.port, :use_ssl => true) do |http|
-      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-      http.ssl_version = :SSLv3
-      http.request(req)
-    end
-    JSON.parse(response.body)['list']
+  def create_client_for account
+    client = Pocket::Client.new(
+      consumer_key: ENV['POCKET_CONSUMER_KEY'],
+      access_token: account.oauth_token
+    )
+    client
   end
 
-  def save_links_on date, oauth_token, user_id, account_id
-    links = user_added_links_on date, oauth_token
-    links.each do |link|
-      unless PocketEntry.exists?(item_id: link[1]['item_id'])
+  def save_bookmarks_created_on date, client, user_id, account_id
+    bookmarks = user_bookmarks_on date, client
+    bookmarks.each do |bookmark|
+      unless PocketEntry.exists?(item_id: bookmark[1]['item_id'])
         PocketEntry.create(
           date: date.to_s[0..9],
           user_id: user_id,
-          item_id: link[1]['item_id'],
-          title: link[1]['resolved_title'],
-          url: link[1]['given_url'],
+          item_id: bookmark[1]['item_id'],
+          title: bookmark[1]['resolved_title'],
+          url: bookmark[1]['given_url'],
           pocket_account_id: account_id
         )
       end
     end
+  end
+
+  def user_bookmarks_on date, client
+    response = client.retrieve(since: date.to_time.to_i)
+    response['list'].blank? ? [] : response['list']
   end
 end
