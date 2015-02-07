01a60c36ca50d7d9310c9c1b1241a38aa3b17dfb
Hamilton Chapman <admin@journalbits.net>
2015-02-07 19:03:54 +0000

    Setup entries being fetched immediately for previous day for service accounts

--- app/models/service_account.rb
+++ app/models/service_account.rb
@@ -1,6 +1,8 @@
 class ServiceAccount < ActiveRecord::Base
   self.abstract_class = true
 
+  after_create :get_yesterdays_entries
+
   def entries constraints = nil
     entry_associations = self.class.reflect_on_all_associations.collect { |assoc|
       assoc.name.to_s.classify
@@ -14,6 +16,18 @@ class ServiceAccount < ActiveRecord::Base
       entries[ass_string.to_sym] = constraints ? self.send(ass_string).where(constraints)
                                                : self.send(ass_string)
       entries
+    end
+  end
+
+  private
+
+  def get_worker account_class
+    (account_class.to_s.split(/(?=[A-Z])/)[0..-2].join("") + "Worker").constantize
+  end
+
+  def get_yesterdays_entries
+    unless self.class.to_s == 'RescueTimeAccount'
+      get_worker(self.class).perform_async(Time.now, self.user_id)
     end
   end
 end
